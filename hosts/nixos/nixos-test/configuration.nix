cef6e3396cd419a192822488809eab922ab66fed
Glenn Brown <glenn.brown@gmail.com>
2025-05-14 22:48:48 -0400

    Trying to fix error:
    Getting this error on a nixos-rebuild switch
    /nix/store/y7r7mi37p552cnygmlk07ahlxd7y09rq-install-systemd-boot.sh: line 3: cp: command not found
    Failed to install bootloader
    warning: error(s) occurred while switching to the new configuration
    
    Found this reddit post with suggested fix.
    https://www.reddit.com/r/NixOS/comments/1fofft8/how_to_use_sed_as_an_extra_install_command_for/

--- hosts/nixos/nixos-test/configuration.nix
+++ hosts/nixos/nixos-test/configuration.nix
@@ -11,7 +11,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.systemd-boot.extraInstallCommands = ''
-    cp -r /boot/* /bootbak/
+    ${pkgs.coreutils}/bin/cp -r /boot/* /bootbak/
   '';
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelModules = [ "drivetemp" ];
